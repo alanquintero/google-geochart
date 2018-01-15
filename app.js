97283426f128e167538e3303ea9905ca94ec3c76
Alan Quintero <alan_q_b@hotmail.com>
2018-01-15 15:57:06 -0600

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-{
-  "name": "Google GeoChart",
-  "description": "A Google GeoChart Sample",
-  "repository": "https://github.com/alan0092/google-geochart",
-  "keywords": ["node", "express", "static"]
-}
